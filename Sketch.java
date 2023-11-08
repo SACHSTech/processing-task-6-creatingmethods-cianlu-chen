5b6a9966dc802ef68638f917877a25847b349be1
cianlu-chen <kalina.chen25@ycdsbk12.ca>
2023-11-08 09:44:19 -0500

    moley

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,45 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    for(float i = 15; i < 600; i += 100){
+       moley(i, 10);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
   }
   
-  // define other methods down here.
+  /**
+   * Draws a moley
+   * @param moleyX sets the X value of the moley
+   * @param moleyY sets the Y value of the moley
+   */
+  public void moley(float moleyX, float moleyY){
+    stroke(0, 0, 0);
+    fill(0, 0, 0);
+    rect(32 + moleyX, 6 + moleyY, 6, 20);
+
+    stroke(49, 34, 160);
+    fill(49, 34, 160);
+    ellipse(35 + moleyX, 46 + moleyY, 70, 60);
+
+    stroke(255, 230, 66);
+    fill(255, 230, 66);
+    ellipse(35 + moleyX, 51 + moleyY, 20, 10);
+    ellipse(35 + moleyX, 4 + moleyY, 8, 8);
+
+    stroke(0, 0, 0);
+    fill(0, 0, 0);
+    ellipse(15 + moleyX, 46 + moleyY, 15, 5);
+    ellipse(55 + moleyX, 46 + moleyY, 15, 5); 
+  }
 }
